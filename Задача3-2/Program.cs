b0814ae0333419b13b1371f807d2d02990d1bf9b
Katerina <m-a-c-u-k-a@mail.ru>
2022-11-16 01:02:04 +0700

    Решение от преподавателя

---
+++ Задача3-2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int remainder = firstNumber % secondNumber;
+
+if(remainder == 0)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine($"Не кратно, {remainder}");
+}
