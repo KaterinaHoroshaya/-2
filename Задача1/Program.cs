817a024ddf13a405e5f709fb02858b8b3751eda9
Katerina <m-a-c-u-k-a@mail.ru>
2022-11-16 00:30:43 +0700

    бОльшая цифра двузначного числа

---
+++ Задача1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+Например:
+78 -> 8
+12-> 2
+85 -> 8 */
+
+int randomNumber = new Random().Next(10,100);
+int lastDigit = randomNumber % 10;
+int firstDigit = randomNumber / 10;
+
+Console.WriteLine(randomNumber);
+if (lastDigit > firstDigit)
+{
+    Console.WriteLine(lastDigit);
+}
+else
+{
+    Console.WriteLine(firstDigit);
+}
