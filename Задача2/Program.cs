ec1355cbbfaf33cf074287609065b8cbb88c3f0e
Katerina <m-a-c-u-k-a@mail.ru>
2022-11-16 00:27:52 +0700

    E,hfnm 2 wbahe

---
+++ Задача2/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+456 -> 46
+782 -> 72
+918 -> 98 */
+
+int randomNumber = new Random().Next(100,1000);
+int lastDigit = randomNumber % 10;
+int firstDigit = randomNumber / 100;
+
+Console.WriteLine(randomNumber);
+
+Console.WriteLine($"{firstDigit} и {lastDigit}");
+
