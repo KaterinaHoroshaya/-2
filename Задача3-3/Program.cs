31dc14d5d67c1d5cc4a39ed57f6d5843fee31dca
Katerina <m-a-c-u-k-a@mail.ru>
2022-11-16 01:21:09 +0700

    Решение от преподавателя с методом

---
+++ Задача3-3/Program.cs
@@ -0,0 +1,21 @@
+﻿int firstNumber = ReadInt("Введите первое число: ");
+int secondNumber = ReadInt("Введите второе число: ");
+
+int remainder = firstNumber % secondNumber;
+
+if(remainder == 0)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine($"{firstNumber} Не кратно {secondNumber}, {remainder}");
+    Console.WriteLine("Не кратно, " + remainder);
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
