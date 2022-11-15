ce260ccf0c81fed819ee72ca3b104f2590f166b9
Katerina <m-a-c-u-k-a@mail.ru>
2022-11-16 02:06:30 +0700

    кратно ли одно другому с методом

---
+++ Задача5-2/Program.cs
@@ -0,0 +1,22 @@
+﻿int firstNumber = ReadInt("Введите первое число: ");
+int secondNumber = ReadInt("Введите первое число: ");
+
+if(Sqr(firstNumber) == secondNumber || Sqr(secondNumber) == firstNumber)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
+int Sqr(int number)
+{
+    return number * number;
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
