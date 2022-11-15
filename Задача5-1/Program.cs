5daa6badeccd752af5e99b320fa7402cace4408e
Katerina <m-a-c-u-k-a@mail.ru>
2022-11-16 02:06:07 +0700

    кратно ли одно другому

---
+++ Задача5-1/Program.cs
@@ -0,0 +1,17 @@
+﻿int firstNumber = ReadInt("Введите первое число: ");
+int secondNumber = ReadInt("Введите первое число: ");
+
+if(firstNumber * firstNumber == secondNumber || secondNumber * secondNumber == firstNumber)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
